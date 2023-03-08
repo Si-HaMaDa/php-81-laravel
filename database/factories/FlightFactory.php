dd7c30b52edbb14197418505a0a0e541af4e2e36
Si HaMaDa <nrookman_27@yahoo.com>
2023-03-08 19:49:15 +0200

    Add Flight Factory Add UserSeeder

---
+++ database/factories/FlightFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Flight>
+ */
+class FlightFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->name(),
+            'number' => fake()->numberBetween(1000, 9999),
+            'color' => fake()->colorName(),
+            'description' => fake()->text($maxNbChars = 300),
+            'active' => fake()->boolean(),
+        ];
+    }
+}
--- database/seeders/FlightSeeder.php
+++ database/seeders/FlightSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Flight;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
@@ -14,14 +15,6 @@ class FlightSeeder extends Seeder
      */
     public function run(): void
     {
-        for ($i = 0; $i < 10; $i++) {
-            DB::table('flights')->insert([
-                'name' => Str::random(10),
-                'number' => rand(1000, 9999),
-                'color' => Str::random(10),
-                'description' => Str::random(100),
-                'active' => rand(0, 1),
-            ]);
-        }
+        Flight::factory(10)->create();
     }
 }
